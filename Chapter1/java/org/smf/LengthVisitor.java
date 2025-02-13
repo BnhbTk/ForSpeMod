66228fab16bf3ccc0ca0209f5ef5829169325a7d
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2025-02-13 20:39:12 +0100

    Create LengthVisitor.java

---
+++ Chapter1/java/org/smf/LengthVisitor.java
@@ -0,0 +1,26 @@
+package org.smf;
+
+/**
+ * This class implements the @Visitor interface to define a visitor that computes the length of the list.
+ */
+class LengthVisitor implements Visitor{
+    /**
+     * The counter of the nodes
+     */
+    private int counter=0;
+
+    /**
+     * The visitor just increments the counter when the node is visited
+     */
+    public void visit(Node n){
+        counter++;
+    }
+
+    /**
+     * returns the value of the counter
+     * @return: the value of the counter
+     */
+    public int get(){
+        return counter;
+    }
+}
